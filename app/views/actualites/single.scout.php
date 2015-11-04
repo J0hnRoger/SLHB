5c412d099ca9352e23c5c6a24695cf92683cb7f3
Quentin Moreau <quentinmoreau@macbook-pro-de-quentin-2.home>
2015-11-04 22:10:53 +0100

    Update readme - salut

--- app/views/actualites/single.scout.php
+++ app/views/actualites/single.scout.php
@@ -3,20 +3,7 @@
 @section('main')
 	@loop
 		<article class="single-article">
-			<div class="article--date">
-				<span>{{ get_the_date('d F Y') }}</span>
-			</div>
-			<span class="article--title"><h2>{{ Loop::title() }}</h2></span>
-			{{ Loop::thumbnail() }}
-			<div class="article--excerpt clearfix">
-				<div class="article--excerpt__content">
-					{{ Loop::content() }}
-					<div class="article--navigation clearfix">
-						<?php previous_post_link('%link', 'Previous'); ?>
-						<?php next_post_link('%link', 'Next'); ?>
-					</div>
-				</div>
-			</div>
+			
 		</article>
 	@endloop
 @stop
