d9c2f295499c670785491d2489510fddd60f92e3
Jonathan ROGER <jroger@expertime.com>
2016-06-07 14:06:45 +0200

    Agenda : Add feature image in detail's cover

--- resources/assets/calendar/core/event.model.js
+++ resources/assets/calendar/core/event.model.js
@@ -7,14 +7,17 @@ angular
 function Event() {
 
   var Event = function (eventObject) {
+    for(var prop in eventObject)
+      this[prop] = eventObject[prop];
+
     this.ID = eventObject.id;
     this.date = moment(eventObject.event_date);
     this.title = eventObject.title.rendered;
     this.content = eventObject.content.rendered;
     if (eventObject.start_time != "") {
       this.startTime = eventObject.start_time;
-
     }
+
     if (eventObject.end_time != "") {
       this.endTime = eventObject.end_time;
     }
