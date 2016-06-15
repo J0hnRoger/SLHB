3ca3c1a50940d7b3caaa0b772c05d206deef3d01
Jonathan ROGER <jroger@expertime.com>
2016-06-15 09:06:53 +0200

    Authentification WP : redirect non-admin to home

--- resources/admin/application.php
+++ resources/admin/application.php
@@ -29,3 +29,44 @@ function allow_contributor_uploads() {
 if (!current_user_can('edit_posts')) {
 	add_filter('show_admin_bar', '__return_false');
 }
+
+//TODO - Authentification/redirect class
+//Redirect simple users on home
+function only_admins_login_area( $redirect_to, $request, $user ) {
+    //hack for redirect function
+    $wpadminUrl =  $redirect_to . '/cms/wp-admin/';
+
+    $autorizedRoles = ['administrator', 'contributor'];
+    if (can_access_to_wpadmin($user)){
+      return $wpadminUrl;
+    }
+
+    return home_url();
+}
+
+add_filter( 'login_redirect', 'only_admins_login_area', 10, 3 );
+
+//... and hide wp-admin for those
+function redirect_user_on_role()
+{
+    $current_user = User::current();
+    if (!can_access_to_wpadmin($current_user)){
+      wp_redirect( home_url() ); exit;
+    }
+}
+
+function can_access_to_wpadmin($user){
+  $redirect = false;
+  $autorizedRoles = ['administrator', 'contributor'];
+  //If login user role is Subscriber
+  if ( isset( $user->roles ) && is_array( $user->roles ) )
+  {
+      foreach ($autorizedRoles as $role) {
+        if(in_array($role, $user->roles))
+            $redirect = true;
+      }
+  }
+  return $redirect;
+}
+
+add_action('admin_init', 'redirect_user_on_role');
