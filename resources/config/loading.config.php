c5606626f2fd3f4a0d7e2cb7b35071f9103b495f
Jonathan ROGER <jroger@expertime.com>
2016-06-22 14:23:36 +0200

    Design - actualites display scroll

--- resources/config/loading.config.php
+++ resources/config/loading.config.php
@@ -8,7 +8,8 @@ return [
     *
     * The key is the namespace and key's value contains one or more paths to your classes.
     */
-    ''  => [themosis_path('theme').'controllers', themosis_path('theme').'models'],
-    'Addons\\' => themosis_path('theme').'addons'
-
+    ''  => [
+            themosis_path('theme').'controllers',
+            themosis_path('theme').'models'],
+            'Addons\\' => themosis_path('theme').'addons'
 ];
